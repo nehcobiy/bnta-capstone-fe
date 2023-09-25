498521c831fcb6b9b2aa47a4a427d9372db9ed05
darkpoetOX <ayindejam2009@yahoo.com>
2023-09-25 17:13:16 +0100

    added actual product images to carousel

--- src/components/HeroImage.js
+++ src/components/HeroImage.js
@@ -2,7 +2,7 @@ import Carousel from 'react-bootstrap/Carousel';
 
 const HeroImage =  () => {
     return (
-        <Carousel fade interval={500}>
+        <Carousel fade interval={1000}>
             <Carousel.Item>
                 <img src={'/Images/heroImage1.jpg'} className={"hero-image"}/>
                 <Carousel.Caption>
@@ -13,6 +13,23 @@ const HeroImage =  () => {
                 <img src={'/Images/heroImage2.jpg'} className={"hero-image"}/>
                 <Carousel.Caption>
 
+                </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+                <img src={'/Images/heroImage3.jpg'} className={"hero-image"}/>
+                <Carousel.Caption>
+
+                </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+                <img src={'/Images/heroImage4.jpg'} className={"hero-image"}/>
+                <Carousel.Caption>
+
+                </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+                <img src={'/Images/heroImage5.jpg'} className={"hero-image"}/>
+                <Carousel.Caption>
 
                 </Carousel.Caption>
             </Carousel.Item>
--- src/containers/HomeContainer.js
+++ src/containers/HomeContainer.js
@@ -1,6 +1,21 @@
+import { useState, useEffect } from "react";
 import HeroImage from "../components/HeroImage";
 
 const HomeContainer = () => {
+
+  const [products, setProducts] = useState([]);
+
+  const fetchProductImages = async () => {
+    const response = await fetch("http://localhost:8080/products");
+    const data = await response.json()
+    setProducts(data);
+    console.log(data);
+  }
+
+  useEffect(() => {
+    fetchProductImages();
+  }, [])
+  
   return (
     <>
   
