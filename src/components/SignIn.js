81c01941cd9ce1b5f2bc3b7ef6c0f915ff3dd098
nimo-mohamed <nimoxmohamed@gmail.com>
2023-09-26 12:59:36 +0100

    created signin modal

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,3 +1,23 @@
-const SignIn = () => {};
+import React from 'react';
+
+const SignIn = ({ handleClose }) => {
+   
+  return (
+    <div>
+      <form action="/action_page.php">
+        <label for="rname">Username:</label><br/>
+        <input type="text" id="rname"/>
+        {/* <input type="submit" value="Enter"/> */}
+        <br/>
+        <label for="fborough">Password:</label><br/>
+        <input type="text" id="fpassword" />
+        {/* <input type="submit" value="Enter"/> */}
+        <br/>
+      </form>
+      <br/>
+      <button onClick={handleClose}>Enter</button>
+    </div>
+  );
+};
 
 export default SignIn;
