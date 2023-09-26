395180d3a21ed389ddadfc259179a26b2f3794e6
nimo-mohamed <nimoxmohamed@gmail.com>
2023-09-26 16:36:28 +0100

    added popular products to home container

--- src/containers/HomeContainer.js
+++ src/containers/HomeContainer.js
@@ -1,31 +1,41 @@
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import HeroImage from "../components/HeroImage";
 import Footer from "../components/Footer";
+import Product from "../components/Product";
 
 const HomeContainer = () => {
-
   const [products, setProducts] = useState([]);
 
-  // Fetching all products below
-  const fetchProductImages = async () => {
-    const response = await fetch("http://localhost:8080/products");
-    const data = await response.json()
-    setProducts(data);
-    console.log(data);
-  }
+  const fetchRandomProducts = async () => {
+    try {
+      const response = await fetch("http://localhost:8080/products/random/9"); // Assuming your API supports a random products endpoint that returns 9 random products
+      const data = await response.json();
+      setProducts(data);
+      console.log(data);
+    } catch (error) {
+      console.error("Error fetching random products:", error);
+    }
+  };
 
   useEffect(() => {
-    fetchProductImages();
-  }, [])
-  
+    fetchRandomProducts();
+  }, []);
+
   return (
     <>
-  
       <div>
-      <HeroImage />
-      <Footer />
+        <HeroImage />
+        <br />
+        <h1>Popular Products</h1>
+        <div className="product-list">
+          {products.map((product) => (
+            <Product key={product.id} product={product} />
+          ))}
+        </div>
+        <Footer />
       </div>
     </>
-)};
+  );
+};
 
 export default HomeContainer;
