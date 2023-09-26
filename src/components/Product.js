3d3c1d3c4e403e519fe58c98a2193af1c39738da
nehcobiy <yibo-chen@hotmail.com>
2023-09-26 10:55:31 +0100

    added quantity functionality

--- src/components/Product.js
+++ src/components/Product.js
@@ -2,9 +2,24 @@ import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
 import Accordion from "react-bootstrap/Accordion";
 import { BsCart } from "react-icons/bs";
+import { useState } from "react";
+import { AiOutlinePlus } from "react-icons/ai";
+import { AiOutlineMinus } from "react-icons/ai";
 
 const Product = ({ product }) => {
   //   console.log(product);
+
+  const [quantity, setQuantity] = useState(1);
+
+  const handlePlus = () => {
+    setQuantity(quantity + 1);
+  };
+
+  const handleMinus = () => {
+    if (quantity > 1) {
+      setQuantity(quantity - 1);
+    }
+  };
 
   return (
     <Card style={{ width: "18rem" }}>
@@ -21,7 +36,14 @@ const Product = ({ product }) => {
         </Card.Text>
         <Card.Text>£{product.price / 100}</Card.Text>
         {/* QUANTITY */}
-        <Button variant="primary">
+        <Button variant="light" size="sm" onClick={handlePlus}>
+          <AiOutlinePlus />
+        </Button>
+        <p>Qty: {quantity}</p>
+        <Button variant="light" size="sm" onClick={handleMinus}>
+          <AiOutlineMinus />
+        </Button>
+        <Button variant="secondary">
           <BsCart />
         </Button>
       </Card.Body>
