88776f7346549c02a5915a03a21bf8264b4df07d
nehcobiy <yibo-chen@hotmail.com>
2023-09-27 10:29:27 +0100

    updated basket

--- src/components/Basket.js
+++ src/components/Basket.js
@@ -2,14 +2,21 @@ import { useState, useContext } from "react";
 import { Button } from "react-bootstrap";
 import { BsCart } from "react-icons/bs";
 import Offcanvas from "react-bootstrap/Offcanvas";
+import Form from "react-bootstrap/Form";
 import { BasketContext } from "../contexts/BasketContext";
 
 const Basket = () => {
   const [show, setShow] = useState(false);
   const { basket, setBasket } = useContext(BasketContext);
+  const [shipping, setShipping] = useState(0);
+  const [basketTotal, setBasketTotal] = useState(0);
+
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
-  console.log(basket);
+
+  const updateShipping = (e) => {
+    setShipping(e.target.value);
+  };
   return (
     <>
       <Button variant="light" size="lg" onClick={handleShow}>
@@ -17,11 +24,31 @@ const Basket = () => {
       </Button>
       <Offcanvas show={show} onHide={handleClose} placement="end">
         <Offcanvas.Header closeButton>
-          <Offcanvas.Title>Shopping Basket</Offcanvas.Title>
+          <Offcanvas.Title as="h2">Shopping Basket</Offcanvas.Title>
         </Offcanvas.Header>
+        <hr />
         <Offcanvas.Body>
-          {/* Products */}
-
+          <Form>
+            <Form.Label>Delivery: </Form.Label>
+            <Form.Select defaultValue="Choose..." onChange={updateShipping}>
+              <option>Choose...</option>
+              <option value={295}>Standard Delivery: £2.95 </option>
+              <option value={395}>Royal Mail Tracked48: £3.95</option>
+              <option value={495}>Royal Mail Tracked24: £4.95</option>
+              <option value={695}>Royal Mail Special Delivery: £6.95</option>
+              <option value={650}>DHL Express: £6.50</option>
+              <option value={1250}>DHL International: £12.50</option>
+            </Form.Select>
+            <Form.Label>Address: </Form.Label>
+            <Form.Select defaultValue="Choose...">
+              <option>Choose...</option>
+              {/* <option>user address</option> */}
+            </Form.Select>
+          </Form>
+          <hr />
+          <p>Cart total: </p>
+          <p>Shipping: £{(shipping / 100).toFixed(2)}</p>
+          <p>Subtotal: </p>
           {/* Form */}
         </Offcanvas.Body>
       </Offcanvas>
